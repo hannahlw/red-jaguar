a04d256d0ecdb765bfb3cc96c74109838d62a075
Hannah <hannah.westheimer@gmail.com>
2015-05-22 15:28:27 -0400

    update show page, controller and front-end styling

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,6 +5,9 @@ class BoardsController < ApplicationController
  
   def show
     @board = Board.find(params[:id])
+    @web_links = @board.links.where(kind: 1)
+    @youtube_links = @board.links.where(kind: 2)
+    @twitter_links = @board.links.where(kind: 3)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'boards#index'
+  
   resources :users
 
   resources :links
