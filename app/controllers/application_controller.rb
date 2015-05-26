a1f6c9f6727521b1212419630d5e73fb64b1dea7
Jonathan Meyers <jmeyers44@gmail.com>
2015-05-26 15:58:56 -0400

    add devise login signup logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
-  
-  def current_user
-    User.find(1)
-  end
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
+     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+   end
 
 end
+
